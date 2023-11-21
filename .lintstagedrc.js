817ad8c42d4c02482b10dc545b10a8f5a01215a3
Jason Ruesch <jruesch@degreed.com>
2023-11-21 10:18:39 -0500

    feat(portfolio): add introduction to the about page

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  '{apps,libs}/**/*.{ts,js,html,json,scss,css,md}': [
+  '{apps,libs}/**/*.{ts,tsx,js,jsx,html,json,scss,css,md}': [
     'nx affected:lint --uncommitted --fix true',
     // 'nx affected:test --uncommitted',
   ],
-  '*.{ts,js,html,json,scss,css,md,yaml,yml}': ['nx format:write --base=main'],
+  '*.{ts,tsx,js,jsx,html,json,scss,css,md,yaml,yml}': [
+    'nx format:write --base=main',
+  ],
 };
--- libs/portfolio/ui/src/lib/about/about.tsx
+++ libs/portfolio/ui/src/lib/about/about.tsx
@@ -2,6 +2,9 @@ export function Component() {
   return (
     <div className="h-full grid place-content-center text-center">
       <h1 className="text-4xl font-bold mb-4">About Me</h1>
+      <p className="text-neutral-500 dark:text-neutral-400">
+        Hi, my name is Jason Ruesch.
+      </p>
     </div>
   );
 }
