e58dba09dbb84a14d76adf1a63c1c485d2137331
Jason Ruesch <jruesch@degreed.com>
2023-11-23 21:25:17 -0500

    feat(portfolio): update footer and add semantic release

---
+++ apps/portfolio/release.config.js
@@ -0,0 +1,46 @@
+const name = 'portfolio';
+const srcRoot = `apps/${name}`;
+
+module.exports = {
+  pkgRoot: `dist/${srcRoot}`,
+  commitPaths: [
+    'nx.json',
+    '.nxignore',
+    'package.json',
+    '.prettierrc',
+    '.prettierignore',
+    `${srcRoot}/*`,
+    `**/${name}/*`,
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: `${srcRoot}/CHANGELOG.md`,
+      },
+    ],
+    '@semantic-release/npm',
+    [
+      '@semantic-release/npm',
+      {
+        pkgRoot: '.',
+        npmPublish: false,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: [
+          'package.json',
+          'package-lock.json',
+          `${srcRoot}/CHANGELOG.md`,
+        ],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
