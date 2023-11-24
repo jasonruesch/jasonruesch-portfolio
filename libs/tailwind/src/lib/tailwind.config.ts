2d5082bf38f81994fc5e2e907accf438626bbbe5
Jason Ruesch <jruesch@degreed.com>
2023-11-23 21:26:11 -0500

    feat(tailwind): add colors

--- libs/tailwind/src/lib/tailwind.config.ts
+++ libs/tailwind/src/lib/tailwind.config.ts
@@ -2,7 +2,22 @@ import type { Config } from 'tailwindcss';
 
 export default {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        tertiary: 'var(--tertiary)',
+        quaternary: 'var(--quaternary)',
+        quinary: 'var(--quinary)',
+        senary: 'var(--senary)',
+        septenary: 'var(--septenary)',
+        octonary: 'var(--octonary)',
+        nonary: 'var(--nonary)',
+        denary: 'var(--denary)',
+        black: 'var(--black)',
+        white: 'var(--white)',
+      },
+    },
   },
   plugins: [],
 } satisfies Omit<Config, 'content'>;
